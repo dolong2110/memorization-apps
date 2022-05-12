6709df23d8c6fe1289b401ada8d71c104da2c1c9
Do Chau Long <dolong2110@gmail.com>
2022-05-13 02:34:25 +0700

    set up docker

--- account/go.mod
+++ account/go.mod
@@ -2,4 +2,4 @@ module github.com/dolong2110/Memoirization-Apps
 
 go 1.16
 
-require github.com/gin-gonic/gin v1.7.1 // indirect
+require github.com/gin-gonic/gin v1.7.1
--- account/main.go
+++ account/main.go
@@ -1,60 +1,59 @@
 package main
 
 import (
-    "context"
-    "log"
-    "net/http"
-    "os"
-    "os/signal"
-    "syscall"
-    "time"
+	"context"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
-    "github.com/gin-gonic/gin"
-
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-    // you could insert your favorite logger here for structured or leveled logging
-    log.Println("Starting server...")
+	// you could insert your favorite logger here for structured or leveled logging
+	log.Println("Starting server...")
 
-    router := gin.Default()
+	router := gin.Default()
 
-    router.GET("/api/account", func(c *gin.Context) {
-        c.JSON(http.StatusOK, gin.H{
-            "hello": "world",
-        })
-    })
+	router.GET("/api/account", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"hello": "long",
+		})
+	})
 
-    srv := &http.Server{
-        Addr:    ":8080",
-        Handler: router,
+	srv := &http.Server{
+		Addr:    ":8080",
+		Handler: router,
 	}
 
-    // Graceful server shutdown - https://github.com/gin-gonic/examples/blob/master/graceful-shutdown/graceful-shutdown/server.go
-    go func() {
-        if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
-            log.Fatalf("Failed to initialize server: %v\n", err)
-        }
-    }()
+	// Graceful server shutdown - https://github.com/gin-gonic/examples/blob/master/graceful-shutdown/graceful-shutdown/server.go
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("Failed to initialize server: %v\n", err)
+		}
+	}()
 
-    log.Printf("Listening on port %v\n", srv.Addr)
+	log.Printf("Listening on port %v\n", srv.Addr)
 
-    // Wait for kill signal of channel
-    quit := make(chan os.Signal)
+	// Wait for kill signal of channel
+	quit := make(chan os.Signal)
 
-    signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
 
-    // This blocks until a signal is passed into the quit channel
-    <-quit
+	// This blocks until a signal is passed into the quit channel
+	<-quit
 
-    // The context is used to inform the server it has 5 seconds to finish
-    // the request it is currently handling
-    ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-    defer cancel()
+	// The context is used to inform the server it has 5 seconds to finish
+	// the request it is currently handling
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
 
-    // Shutdown server
-    log.Println("Shutting down server...")
-    if err := srv.Shutdown(ctx); err != nil {
-        log.Fatalf("Server forced to shutdown: %v\n", err)
+	// Shutdown server
+	log.Println("Shutting down server...")
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatalf("Server forced to shutdown: %v\n", err)
 	}
 }
