01c40b2bf63b80eac4f389b5172aa957c8bb4e2c
Do Chau Long <dolong2110@gmail.com>
2022-05-17 22:39:57 +0700

    fix bugs

--- account/utils/token.go
+++ account/utils/token.go
@@ -27,9 +27,7 @@ func GenerateIDToken(user *model.User, key *rsa.PrivateKey, exp int64) (string, 
 	}
 
 	token := jwt.NewWithClaims(jwt.SigningMethodRS256, claims)
-	log.Printf("KEYS: ", key)
 	ss, err := token.SignedString(key)
-	log.Printf("KEY STR: " + ss)
 	if err != nil {
 		log.Println("Failed to sign id token string")
 		return "", err
